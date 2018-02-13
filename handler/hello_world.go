a83c2f6a60c6ea5b8adfbca35768dc02696a2ad9
nasum <masaya.nasu@crowdworks.co.jp>
2018-02-13 16:24:27 +0900

    output header

--- handler/hello_world.go
+++ handler/hello_world.go
@@ -1,7 +1,9 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo"
 )
@@ -9,6 +11,9 @@ import (
 // HelloWorld hello world と返すだけのメソッド
 func HelloWorld() echo.HandlerFunc {
 	return func(c echo.Context) error {
+		output := fmt.Sprintf("%#v", c.Request().Header)
+
+		os.Stdout.Write([]byte(output + "\n"))
 		return c.String(http.StatusOK, "Hello World")
 	}
 }
